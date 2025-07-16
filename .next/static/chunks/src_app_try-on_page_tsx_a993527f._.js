40b97781ccce7acc767b8dc5e69e662c00a53953
Evhanela <tabanliza528@gmail.com>
2025-07-16 20:10:27 +0400

    Add .next/static/chunks/src_app_try-on_page_tsx_a993527f._.js 1752682227900

---
+++ .next/static/chunks/src_app_try-on_page_tsx_a993527f._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_d9c41914._.js"
+],
+    source: "dynamic"
+});
